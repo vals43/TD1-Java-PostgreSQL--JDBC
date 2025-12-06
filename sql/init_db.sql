520d09cdd7c7fab3b43986fecb0645827162d0c9
Vals43 <hei.teddy.3@gmail.com>
2025-12-06 13:47:04 +0300

    setup sql

---
+++ sql/init_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE product_management_db;
+
+CREATE USER product_manager_user WITH PASSWORD '123456';
+
+GRANT ALL PRIVILEGES ON DATABASE product_management_db TO product_manager_user;
---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    price NUMERIC(10,2),
+    creation_datetime TIMESTAMP
+);
+
+CREATE TABLE product_category (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    product_id INT REFERENCES product(id)
+);
